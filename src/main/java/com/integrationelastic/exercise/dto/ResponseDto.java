6f279d81c24fd96fce4619a19cb57958a02dc493
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-03-12 15:03:36 -0500

    [FEATURE] Add dro, exceptions, model, repository

--- src/main/java/com/integrationelastic/exercise/dto/ResponseDto.java
+++ src/main/java/com/integrationelastic/exercise/dto/ResponseDto.java
@@ -1,4 +1,12 @@
 package com.integrationelastic.exercise.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ResponseDto {
+    private String message;
 }
--- src/main/java/com/integrationelastic/exercise/repository/IEmployeeRepository.java
+++ src/main/java/com/integrationelastic/exercise/repository/IEmployeeRepository.java
@@ -2,6 +2,12 @@ package com.integrationelastic.exercise.repository;
 
 import com.integrationelastic.exercise.entity.Employee;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
-public interface IEmployeeRepository extends ElasticsearchRepository<Employee, Long> {
+import java.util.List;
+
+@Repository
+public interface IEmployeeRepository extends ElasticsearchRepository<Employee, String> {
+    // Metodo para retornar todos
+    List<Employee> findAll();
 }
